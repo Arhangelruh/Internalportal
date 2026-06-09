3ca951a947633104c5a0481cf6a770e59e054e4f
Ruh <ruh@tut.by>
2026-06-09 09:38:02 +0300

    init: add dto model to collect data about reviewing.

---
+++ src/InternalPortal.Core/Models/ReviewDto.cs
@@ -0,0 +1,35 @@
+﻿namespace InternalPortal.Core.Models
+{
+	public class ReviewDto
+	{
+		/// <summary>
+		/// Review record id.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// File name.
+		/// </summary>
+		public string FileName { get; set; }
+
+		/// <summary>
+		/// User Name.
+		/// </summary>
+		public string UserName { get; set; }
+
+		/// <summary>
+		/// Last name.
+		/// </summary>
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// Middle name.
+		/// </summary>
+		public string? MiddleName { get; set; }
+
+		/// <summary>
+		/// Time when user confirm reading the file.
+		/// </summary>
+		public DateTime ConfirmTime { get; set; }
+	}
+}
